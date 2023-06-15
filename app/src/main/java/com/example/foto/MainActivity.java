259a6f091ed1c45ca903b01228ee8dab591a1cad
jimmy1724524143 <jeims-sk@live.com>
2023-06-15 07:44:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/foto/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.foto;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView imageView;
+    private Bitmap imageBitmap;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.img_pic);
+    }
+
+    public void takePicture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void saveImage(View view) {
+        if (imageBitmap != null) {
+            String savedImageURL = MediaStore.Images.Media.insertImage(
+                    getContentResolver(),imageBitmap,"Imagen capturada","Descripción de la imagen");
+            Toast.makeText(this,"GUARDADO CORRECTO!",Toast.LENGTH_LONG).show();
+            if (savedImageURL != null) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+
+        }
+
+    }
+
+}
